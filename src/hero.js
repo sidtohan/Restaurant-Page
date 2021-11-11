0946a7a9dc470c8cf18b6401da4a8b0f286927db
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-11-11 11:24:21 +0530

    Staged the new files properly

---
+++ src/hero.js
@@ -0,0 +1,44 @@
+import KnifeFork from './knife-fork.svg'
+
+const generateHero = (() => {
+  const generateLeftPart = () => {
+
+    const leftPart = document.createElement('div');
+    leftPart.classList.add('left-part-hero');
+
+    const subTitle = document.createElement('div');
+    subTitle.textContent = "The Top Restaurant in XYZ City";
+    subTitle.classList.add('sub-title');
+
+    const title = document.createElement('div');
+    title.textContent = "We are the ultimate food hub for foodies and critics alike";
+    title.classList.add('title') ;
+
+    const letsEat = document.createElement('button');
+    letsEat.classList.add('lets-eat');
+
+    const letsEatText = document.createElement('div');
+    letsEatText.classList.add('lets-eat-text');
+    letsEatText.textContent = "Lets Eat";
+
+    const knifeFork = document.createElement('img');
+    knifeFork.classList.add('knife-fork');
+    knifeFork.src = KnifeFork;
+
+    letsEat.appendChild(letsEatText);
+    letsEat.appendChild(knifeFork);
+
+    leftPart.appendChild(subTitle);
+    leftPart.appendChild(title);
+    leftPart.appendChild(letsEat);
+
+    return leftPart;
+  }
+
+  return {
+    generateLeftPart,
+  }
+})()
+
+export default generateHero;
+
