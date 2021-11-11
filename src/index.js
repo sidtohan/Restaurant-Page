92b57c562c20e5ac87606439544c06453ea823b8
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-11-11 16:07:32 +0530

    Testing something

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import generateHero from './modules/hero-home';
 import './styles/style.css';
 import './styles/nav.css'
 import './styles/hero.css';
+import { cli } from 'webpack';
 
 const hero = document.createElement('div');
 const nav = document.createElement('div');
 const footer = document.createElement('div');
+
+let current = "Home";
 
 hero.id = "hero";
 nav.id = "nav";
@@ -14,14 +17,25 @@ footer.id = "footer";
 
 // Call the fucntions tab functions here, to add text content and stuff to
 // these divs
-const homePage = () => {
+const home = () => {
   // nav
   nav.appendChild(generateNav.generateHeading());
   nav.appendChild(generateNav.generateNavBar());
   const tabs = nav.querySelector('.nav-links').children;
   for (let tab of tabs) {
     tab.addEventListener('click', e => {
-      console.log(e.target);
+      let clicked  = e.target.textContent;
+      if(clicked === current){
+        // do nothing
+        return;
+      }
+      if(clicked === 'Home'){
+        home();
+      } else if (clicked === "Menu"){
+        console.log("menu");
+      } else{
+        console.log("Credits");
+      }
       e.stopPropagation();
     })
   }
@@ -31,7 +45,7 @@ const homePage = () => {
   hero.appendChild(generateHero.generateRightPart());
 }
 
-homePage();
+home();
 
 document.body.appendChild(nav);
 document.body.appendChild(hero);
