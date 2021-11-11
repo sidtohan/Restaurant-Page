633bcbeead22dfc6251c7b77234730bd346f6be0
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-11-11 11:23:25 +0530

    Made progress on the left part of the hero section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import generateNav from './nav'
-import './style.css'
+import generateNav from './nav';
+import generateHero from './hero';
+import './styles/style.css';
+import './styles/hero.css';
 
 const hero = document.createElement('div');
 const nav = document.createElement('div');
@@ -12,11 +14,17 @@ footer.id = "footer";
 // Call the fucntions tab functions here, to add text content and stuff to
 // these divs
 const homePage = () => {
+  // nav
   nav.appendChild(generateNav.generateHeading());
   nav.appendChild(generateNav.generateNavBar());
+
+  // hero 
+  hero.appendChild(generateHero.generateLeftPart());
+
 }
 
 homePage();
+
 document.body.appendChild(nav);
 document.body.appendChild(hero);
 document.body.appendChild(footer)
