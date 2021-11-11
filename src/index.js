ce13d9b45473b09a8af331b1b22350b0426f964d
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-11-11 16:26:27 +0530

    Added the base logic for tab selection

--- src/index.js
+++ src/index.js
@@ -3,50 +3,62 @@ import generateHero from './modules/hero-home';
 import './styles/style.css';
 import './styles/nav.css'
 import './styles/hero.css';
-import { cli } from 'webpack';
-
-const hero = document.createElement('div');
-const nav = document.createElement('div');
-const footer = document.createElement('div');
 
 let current = "Home";
-
-hero.id = "hero";
-nav.id = "nav";
-footer.id = "footer";
 
 // Call the fucntions tab functions here, to add text content and stuff to
 // these divs
 const home = () => {
+  // creating elements
+  const hero = document.createElement('div');
+  const nav = document.createElement('div');
+
+  hero.id = "hero";
+  nav.id = "nav";
+
   // nav
   nav.appendChild(generateNav.generateHeading());
   nav.appendChild(generateNav.generateNavBar());
-  const tabs = nav.querySelector('.nav-links').children;
-  for (let tab of tabs) {
-    tab.addEventListener('click', e => {
-      let clicked  = e.target.textContent;
-      if(clicked === current){
-        // do nothing
-        return;
-      }
-      if(clicked === 'Home'){
-        home();
-      } else if (clicked === "Menu"){
-        console.log("menu");
-      } else{
-        console.log("Credits");
-      }
-      e.stopPropagation();
-    })
-  }
 
   // hero 
   hero.appendChild(generateHero.generateLeftPart());
   hero.appendChild(generateHero.generateRightPart());
+
+
+  // appending the requiured sections
+  document.body.appendChild(nav);
+  document.body.appendChild(hero);
 }
 
-home();
+const tabClicks = () => {
+  const tabs = nav.querySelector('.nav-links').children;
+  for (let tab of tabs) {
+    tab.addEventListener('click', e => {
+      let clicked = e.target.textContent;
+      if (clicked === current) {
+        // do nothing
+        return;
+      }
 
-document.body.appendChild(nav);
-document.body.appendChild(hero);
-document.body.appendChild(footer)
+      if (clicked === 'Home') {
+        document.body.innerHTML = '';
+        home();
+        tabClicks();
+        return;
+
+      } else if (clicked === "Menu") {
+        console.log("Menu");
+        current = clicked;
+        return;
+      } else {
+        console.log("Credits");
+        current = clicked;
+        return;
+      }
+    })
+  }
+}
+
+// generate homepage by default
+window.onload = home();
+window.onload = tabClicks();
